a11e8585854da49dda0a4863ef3742dabcfa6062
brendanwallison <57687858+brendanwallison@users.noreply.github.com>
2023-12-07 10:16:15 -0600

    Update sunrise_sunset.py
    Allow multi-year generation

--- sunrise_sunset.py
+++ sunrise_sunset.py
@@ -3,6 +3,7 @@ import datetime
 import calendar
 import pytz
 import tzlocal
+import sys
 
 
 URL = "https://api.sunrise-sunset.org/json?lat=30.087479&lng=-97.169851&formatted=0&date="
@@ -13,10 +14,11 @@ URL = "https://api.sunrise-sunset.org/json?lat=30.087479&lng=-97.169851&formatte
 # in a manner that exceeds reasonable request volume, constitutes excessive or
 # abusive usage. We require that you show attribution to us with a link to our site.
 
-today = datetime.date.today()
-
-## Get the current year
-year = today.year
+if len(sys.argv) == 1:
+    years = [datetime.date.today().year]
+else:
+    years = map(int, sys.argv[1:])
+    
 
 ## Get the local time zone
 local_timezone = tzlocal.get_localzone()
@@ -24,16 +26,17 @@ local_timezone = tzlocal.get_localzone()
 r = requests.get(url = URL)
 data = r.json()
 cal = calendar.Calendar()
-for month in range(1,13):
-    for date in cal.itermonthdates(year, month):
-        check = URL + str(date)
-        r = requests.get(url = check)
-        data = r.json()
-        sunrise = datetime.datetime.strptime(data['results']['sunrise'][0:19],'%Y-%m-%dT%H:%M:%S')
-        sunset = datetime.datetime.strptime(data['results']['sunset'][0:19],'%Y-%m-%dT%H:%M:%S')
-        ## convert sunrise and sunset from UTC to local timezone
-        sunrise = sunrise.replace(tzinfo=pytz.utc).astimezone(local_timezone)
-        sunset = sunset.replace(tzinfo=pytz.utc).astimezone(local_timezone)
-
-        print ('{} {} {}'.format(sunrise.strftime('%m/%d/%Y'),sunrise.strftime('%I:%M %p'),sunset.strftime('%I:%M %p')))
-        #print(date)
+for year in years:
+    for month in range(1,13):
+        for date in cal.itermonthdates(year, month):
+            check = URL + str(date)
+            r = requests.get(url = check)
+            data = r.json()
+            sunrise = datetime.datetime.strptime(data['results']['sunrise'][0:19],'%Y-%m-%dT%H:%M:%S')
+            sunset = datetime.datetime.strptime(data['results']['sunset'][0:19],'%Y-%m-%dT%H:%M:%S')
+            ## convert sunrise and sunset from UTC to local timezone
+            sunrise = sunrise.replace(tzinfo=pytz.utc).astimezone(local_timezone)
+            sunset = sunset.replace(tzinfo=pytz.utc).astimezone(local_timezone)
+    
+            print ('{} {} {}'.format(sunrise.strftime('%m/%d/%Y'),sunrise.strftime('%I:%M %p'),sunset.strftime('%I:%M %p')))
+            #print(date)
